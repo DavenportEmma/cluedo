23f4f71fa00259b98e4f746c4f48c4872caeb935
ConorDavenport <cnrdavenport@gmail.com>
2019-07-30 00:02:14 +0100

    didn't really do anything tbh

--- js/cluedo.js
+++ js/cluedo.js
@@ -3,8 +3,12 @@ var suspects = ["scarlet","green","mustard","plum","peacock","white"];
 var weapons = ["candlestick","dagger","pipe","revolver","rope","spanner"];
 var rooms = ["kitchen","ballroom","conservatory","billiard","library","study","hall","lounge","dining"];
 
+var guess = {
+	
+};
+
 // prevents scripts from running before elements have loaded
 window.onload = function() 
 {
-	document.getElementById("suspectList").innerHTML = suspects;
+	
 }
