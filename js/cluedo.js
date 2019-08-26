c96f28219dbf070c1db286c379c67713e2f82f5d
ConorDavenport <cnrdavenport@gmail.com>
2019-08-27 00:35:19 +0100

    added collapsible sections

--- js/cluedo.js
+++ js/cluedo.js
@@ -751,6 +751,25 @@ window.onbeforeunload = function()
 // add event listeners here
 window.onload = function() 
 {
+	var coll = document.getElementsByClassName("collapsible");
+	var i;
+
+	for (i = 0; i < coll.length; i++) 
+	{
+		coll[i].addEventListener("click", function() 
+		{
+			this.classList.toggle("active");
+			var content = this.nextElementSibling;
+			if(content.style.display === "block") 
+			{
+				content.style.display = "none";
+			} 
+			else 
+			{
+				content.style.display = "block";
+			}
+		});
+	}
 	document.getElementById("playerNumberSubmit").addEventListener("click",playerNumberUpdate);
 	document.getElementById("yesCardSubmit").addEventListener("click",
 		function()
